19ef3d15a0b1cb28eb29e2f274c97a9d22c3e784
LeoYulinLi <LeoYulinLi@gmail.com>
2020-07-14 11:11:57 -0700

    Initial work on auto zoom

--- src/nodes/network/NetworkPanel.ts
+++ src/nodes/network/NetworkPanel.ts
@@ -10,6 +10,8 @@ export default class NetworkPanel {
 
   private lastClickPosition: Coordinate = { x: 0, y: 0 };
 
+  private nodeHandles: paper.Shape[] = [];
+
   private layers = {
     marquee: new paper.Layer(),
     handles: new paper.Layer(),
@@ -20,26 +22,45 @@ export default class NetworkPanel {
   constructor(private network: Network) {
     network.events.on("neuronAdded", this.addNeuron.bind(this));
 
-    this.project.addLayer(this.layers.marquee);
-    this.project.addLayer(this.layers.handles);
-    this.project.addLayer(this.layers.nodes);
     this.project.addLayer(this.layers.connections);
+    this.project.addLayer(this.layers.nodes);
+    this.project.addLayer(this.layers.handles);
+    this.project.addLayer(this.layers.marquee);
 
     const tool = new paper.Tool();
     tool.onMouseUp = (event: paper.MouseEvent) => {
       this.lastClickPosition = event.point;
+      this.project.view.zoom = this.preferredZoomLevel;
+      this.project.view.center = this.layerBound.center;
+    };
+
+    tool.onMouseDown = () => {
+      this.nodeHandles.forEach(r => r.remove());
     };
 
     tool.onKeyDown = (event: paper.KeyEvent) => {
       switch (event.key) {
-      case "p": network.createNeuron({ coordinate: this.lastClickPosition });
+      case "p":
+        network.createNeuron({ coordinate: this.lastClickPosition });
+        break;
       }
     };
   }
 
+  get preferredZoomLevel(): number {
+    if (this.layerBound.isEmpty()) return 1;
+    return Math.min(
+      this.project.view.viewSize.width / this.layerBound.width,
+      this.project.view.viewSize.height / this.layerBound.height
+    );
+  }
+
+  get layerBound(): paper.Rectangle {
+    return this.layers.nodes.bounds.scale(1.1);
+  }
+
   private addNeuron(neuron: Neuron) {
     const neuronNode = new NeuronNode(neuron);
-    console.log(this.network.neurons);
     this.layers.nodes.addChild(neuronNode.node);
     neuron.events.on("delete", () => {
       neuronNode.node.remove();
@@ -49,5 +70,12 @@ export default class NetworkPanel {
       neuronNode.node.position = new paper.Point(x, y);
     });
     neuronNode.node.onMouseDown = () => neuronNode.node.bringToFront();
+    neuronNode.events.on("click", () => {
+      const rect = new paper.Shape.Rectangle(neuronNode.node.bounds);
+      rect.scale(1.2);
+      rect.strokeColor = new paper.Color("green");
+      this.nodeHandles.push(rect);
+      this.layers.handles.addChild(rect);
+    });
   }
 }
--- src/nodes/network/NeuronNode.ts
+++ src/nodes/network/NeuronNode.ts
@@ -1,21 +1,26 @@
 import { Neuron } from "../../model/network/Neuron";
 import paper from "paper";
+import eventEmitter from "../../events/emitter";
 
 export class NeuronNode {
 
   private number = new paper.PointText(new paper.Point(0, 0));
 
-  private circle = new paper.Shape.Circle(new paper.Point(0, 0), 20);
+  private circle = new paper.Shape.Circle(new paper.Point(0, 0), 12);
 
   private _node = new paper.Group([this.circle, this.number]);
 
+  readonly events = eventEmitter<{
+    click: NeuronNode
+  }>()
+
   constructor(private neuron: Neuron) {
-    this.circle.strokeWidth = 2;
     this.circle.strokeColor = new paper.Color("black");
     this.circle.fillColor = new paper.Color("#ffffffaa");
     this.number.content = this.neuron.value.toPrecision(1);
     this.number.justification = "center";
-    this.number.fontSize = 16;
+    this.number.fontSize = 11;
+    this.number.fontFamily = "Arial";
     this.number.translate(new paper.Point(0, - this.number.position.y));
     neuron.events.on("value", value => {
       this.number.content = value.toPrecision(1);
@@ -25,6 +30,10 @@ export class NeuronNode {
       neuron.coordinate = event.point;
     };
     this.node.position = new paper.Point(neuron.coordinate);
+
+    // "redirect" clicks to the main node
+    this.circle.onClick = () => this.events.fire("click", this);
+    this.number.onClick = () => this.events.fire("click", this);
   }
 
   get node(): paper.Group {
