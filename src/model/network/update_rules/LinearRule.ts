19bd581f811e39226bfbfc7e8364f85cab91fd95
LeoYulinLi <LeoYulinLi@gmail.com>
2020-07-17 14:59:37 -0700

    Fixed graphical bounds

--- src/model/network/update_rules/LinearRule.ts
+++ src/model/network/update_rules/LinearRule.ts
@@ -8,10 +8,6 @@ export class LinearRule extends NeuronUpdateRule implements BoundedUpdateRule {
   lowerBound = -1;
   upperBound = 1;
   bias = 0;
-
-  readonly graphicalLowerBound: number = this.lowerBound;
-  readonly graphicalNeutral: number = (this.upperBound - this.lowerBound) / 2 + this.lowerBound;
-  readonly graphicalUpperBound: number = this.upperBound;
   
   constructor(options: Pick<Partial<LinearRule>, 'lowerBound' | 'upperBound' | 'bias'>) {
     super();
@@ -20,6 +16,18 @@ export class LinearRule extends NeuronUpdateRule implements BoundedUpdateRule {
 
   compute(neuron: Neuron): number {
     return clamp(neuron.bufferedValue, this.lowerBound, this.upperBound) + this.bias;
+  }
+
+  get graphicalLowerBound(): number{
+    return this.lowerBound;
+  }
+
+  get graphicalNeutral(): number {
+    return (this.upperBound - this.lowerBound) / 2 + this.lowerBound;
+  }
+
+  get graphicalUpperBound(): number {
+    return this.upperBound;
   }
 
 }
